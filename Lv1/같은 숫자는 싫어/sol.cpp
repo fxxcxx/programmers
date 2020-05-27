fab8b3213984aa3b054721346afbeea6b8e57e56
Ming <63344889+fxxcxx@users.noreply.github.com>
2020-05-27 10:05:44 +0900

    Update sol.cpp

--- Lv1/같은 숫자는 싫어/sol.cpp
+++ Lv1/같은 숫자는 싫어/sol.cpp
@@ -10,7 +10,7 @@ vector<int> solution(vector<int> arr)
     
     for(int i=1; i<arr.size(); i++){ //사이즈 조건 <= 가 아닌 <
         if(arr[i]!=arr[i-1])
-            answer.push_back(arr[i]);
+            answer.push_back(arr[i]); //push_back 함수를 몰라서 answer에 값을 못넣었다. vector 멤버함수 조심
     }
     return answer;
 }
