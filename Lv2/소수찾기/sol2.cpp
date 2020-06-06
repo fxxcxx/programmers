04ab25e7eb46b2f8a222d385dff462b4212d77dd
Ming <63344889+fxxcxx@users.noreply.github.com>
2020-06-06 14:40:23 +0900

    주석추가
    객관적으로 이게 더 이해 잘됨

--- Lv2/소수찾기/sol2.cpp
+++ Lv2/소수찾기/sol2.cpp
@@ -16,18 +16,18 @@ void isPrime(int num){
         arr[i] = true; //빼먹음
     
     for(int i=2; i<=num; i++){
-        if (arr[i])//true이면
+        if (arr[i])
             for(int j=i+i; j<=num; j+=i)
                 arr[j] = false;
     }
 }
 
 bool isNum(int i, string num){
-    string abc = to_string(i);
+    string abc = to_string(i); //어차피 두개는 string, 문자열로 비교할것임
     for(int i=0; i<abc.size(); i++){
         int flag = false;
         for(int j=0; j<num.size(); j++){
-            if(abc.at(i) == num.at(j)){ //at
+            if(abc.at(i) == num.at(j)){ //at 넣는거 까먹지 말고! at은 소괄호 쓴다.
                 flag = true;
                 num.at(j) = ' ';
                 break;
@@ -43,11 +43,11 @@ int solution(string numbers) {
     int answer = 0;
     
     sort(numbers.begin(), numbers.end(), greater<int>());
-    int max = stoi(numbers);
+    int max = stoi(numbers); //max까지의 소수 판별을 위해 string을 int로 바꿈
     
     isPrime(max);
     for(int i=0; i<=max; i++ ){
-        if(arr[i] && isNum(i,numbers) )
+        if(arr[i] && isNum(i,numbers) ) //arr 배열의 TF를 비교
         answer++;
     }
     return answer;
