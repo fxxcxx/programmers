ba4a635077c0ce181dc91463a53ee5797327508e
Ming <63344889+fxxcxx@users.noreply.github.com>
2020-07-19 16:03:30 +0900

    Create sol.cpp

---
+++ Lv1/제일 작은 수 제거하기/sol.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    
+   arr.erase(min_element(arr.begin(), arr.end() ));
+  
+    if(arr.empty()){
+        answer.push_back(-1);
+    }
+    else answer = arr;
+    
+    return answer;
+}
